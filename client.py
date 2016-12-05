87af8aa4491c78b738b76c718803773f7d95b8e8
Brad McCausland <mccausb@students.wwu.edu>
2016-12-04 21:48:55 -0800

    Details

--- client.py
+++ client.py
@@ -1,19 +1,37 @@
+# Bradley McCausland - W01053708
+# CS578 - Rrushi
+# 12/7/2016
+# Assignment 4 - Client
+
+import sys
 from socket import *
+from methods import *
+from random import getrandbits
+from time import sleep
+
 from Crypto.Cipher import AES
 from Crypto.Util import Counter
 from Crypto import Random
-from methods import *
-from random import getrandbits
 
-s = socket()
-host = gethostname()
+DEBUG = False
 port = 8001
+s    = socket()
+host = gethostname()
 
-#private key
+try:
+	if 'd' in sys.argv[1]:
+		DEBUG = True
+	if 's' in sys.argv[1]:
+		port = input("Enter a port number: ")
+		host = input("Enter an IP address: ")
+except:
+	pass
+
+#randomize private key
 k = getrandbits(32)
 
-#AES key
-x = 1234567890
+#randomize AES key
+x = getrandbits(32)
 
 #defined constants
 p = 4094027087
@@ -24,36 +42,35 @@ message = raw_input('Connection established. Enter message: ')
 
 s.connect((host, port))
 
-alpha = int(s.recv(8001))
-print "alpha recieved: ",alpha
-beta  = int(s.recv(8001))
-print "beta recieved: ",beta
+#recieve public keys
+alpha = int(s.recv(port))
+beta  = int(s.recv(port))
 
 y1 = square_and_multiply(alpha, k, p)
-
 s.send(str(y1))
-print "sent y1"
 
 y2 = square_and_multiply(beta, k, p)
 
-print "Y2: ",y2
+#space out send commands. This seems to prevent a bug.
+sleep(0.1)
 
+#encrypt AES key
 ciphertext = (y2 * x) % p
-print "Ciphertext: ",str(ciphertext)
 s.send(str(ciphertext))
 
-print "length: ", len(str(x))
-
+#format key to 32 bits
 x = format(x, '#034b')[2:]
-#AES may only work with 16 bits
-print x
-#key = passwordToAesKey(str(x))
-ctr = Counter.new(128)
-cipher = AES.new(x, AES.MODE_CTR, counter=ctr)
+
+#init cryptosystem
+ctr     = Counter.new(128)
+cipher  = AES.new(x, AES.MODE_CTR, counter=ctr)
 message = cipher.encrypt(message)
 
 s.send(message)
 
-s.close()
+if DEBUG:
+	print "Sending y1: ", str(y1)
+	print "Sending encrypted AES key: ", str(ciphertext)
+	print "Sending encrypted message: ", message
 
-print "Message sent to localhost port 8001"
+s.close()
--- methods.py
+++ methods.py
@@ -1,6 +1,9 @@
-#helper functions for assignment 4
-import sys, hashlib, string, getpass, binascii, struct, cPickle
+# Bradley McCausland - W01053708
+# CS578 - Rrushi
+# 12/7/2016
+# Assignment 4 - Utility Methods
 
+#Greatest common divisor
 def gcd(a, b):
     if a == 0:
         return (b, 0, 1)
@@ -8,6 +11,7 @@ def gcd(a, b):
         g, y, x = gcd(b%a, a)
         return (g, x - (b//a) * y, y)
 
+#modular inverse
 def modinv(a, m):
     g, x, y = gcd(a, m)
     if g != 1:
@@ -20,29 +24,10 @@ def modinv(a, m):
 def square_and_multiply(m, t, n):
 	z = 1
 	y = m
-	binary = bin(t)[2:] #or something
-
-	while len(binary) != 32:
-		binary = '0' + binary
+	binary = format(t, '#034b')[2:]
 
 	for i in reversed(range(1, len(binary))):
 		if binary[i] == "1":
 			z = (z*y)%n
 		y = (y*y)%n
 	return z
-
-#From assignment 2
-def passwordToAesKey(password):
-    sha256 = hashlib.sha256()
-    sha256.update(password)
-    
-    aesKey = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
-
-    el = 0
-    for c in list(sha256.digest()):
-        if el < 16:
-           aesKey[el] = ord(c)
-           el = el+1
-        
-    key = (aesKey[0]<<120) | (aesKey[1]<<112) | (aesKey[2]<<104) | (aesKey[3]<<96) | (aesKey[4]<<88) | (aesKey[5]<<80) | (aesKey[6]<<72) | (aesKey[7]<<64) | (aesKey[8]<<56) | (aesKey[9]<<48)  | (aesKey[10]<<40) | (aesKey[11]<<32) | (aesKey[12]<<24) | (aesKey[13]<<16) | (aesKey[14]<<8) | (aesKey[15]<<0) 
-    return str(key)
